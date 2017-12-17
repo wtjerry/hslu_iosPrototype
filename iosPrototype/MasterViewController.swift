7394ec05e01ec6d5ffbf2ac57caeb21f4ac229b0
wtjerry <[omitted]>
2017-12-17 13:31:13 +0100

    post working with a draft implementation

--- iosPrototype/MasterViewController.swift
+++ iosPrototype/MasterViewController.swift
@@ -35,6 +35,56 @@ class MasterViewController: UITableViewController, UISearchBarDelegate {
                 print(error)
             }
             self.fetchFeedbacksFromDatabase()
+            
+            
+            
+            
+
+            let parameters = ["text": "hello from ios", "votes": "123"] as [String : Any]
+            
+            //create the url with URL
+            let url = URL(string: "https://fischli.pythonanywhere.com/postJSONparam/")!
+            
+            //create the session object
+            let session = URLSession.shared
+            
+            //now create the URLRequest object using the url object
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST" //set http method as POST
+            
+            do {
+                request.httpBody = try JSONSerialization.data(withJSONObject: parameters)
+            } catch let error {
+                print(error.localizedDescription)
+            }
+            
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+            
+            //create dataTask using the session object to send data to the server
+            let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+                
+                guard error == nil else {
+                    return
+                }
+                
+                guard let data = data else {
+                    return
+                }
+                
+                do {
+                    //create json object from data
+                    if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                        print(json)
+                        // handle json...
+                    }
+                } catch let error {
+                    print(error.localizedDescription)
+                }
+            })
+            task.resume()
+            
+            
         }
     }
     override func viewWillAppear(_ animated: Bool) {
@@ -62,7 +112,7 @@ class MasterViewController: UITableViewController, UISearchBarDelegate {
     }
     
     func fetchJSONToDatabaseAndUpdateUI() {
-        let urlString = "https://fischli.pythonanywhere.com"
+        let urlString = "https://fischli.pythonanywhere.com/getJSONwithparam/"
         let url = URL(string: urlString)
         URLSession.shared.dataTask(with:url!) { (data, response, error) in
             if error == nil {
