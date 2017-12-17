b58bae3316816bfed8f755d237027c8c38d8f7f7
wtjerry <[omitted]>
2017-12-17 15:54:21 +0100

    pushing now the actual created feedback to server instead of dummy data.

--- iosPrototype/MasterViewController.swift
+++ iosPrototype/MasterViewController.swift
@@ -36,55 +36,7 @@ class MasterViewController: UITableViewController, UISearchBarDelegate {
             }
             self.fetchFeedbacksFromDatabase()
             
-            
-            
-            
-
-            let parameters = ["text": "hello from ios", "votes": "123"] as [String : Any]
-            
-            //create the url with URL
-            let url = URL(string: "https://fischli.pythonanywhere.com/postJSONparam/")!
-            
-            //create the session object
-            let session = URLSession.shared
-            
-            //now create the URLRequest object using the url object
-            var request = URLRequest(url: url)
-            request.httpMethod = "POST" //set http method as POST
-            
-            do {
-                request.httpBody = try JSONSerialization.data(withJSONObject: parameters)
-            } catch let error {
-                print(error.localizedDescription)
-            }
-            
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.addValue("application/json", forHTTPHeaderField: "Accept")
-            
-            //create dataTask using the session object to send data to the server
-            let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
-                
-                guard error == nil else {
-                    return
-                }
-                
-                guard let data = data else {
-                    return
-                }
-                
-                do {
-                    //create json object from data
-                    if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
-                        print(json)
-                        // handle json...
-                    }
-                } catch let error {
-                    print(error.localizedDescription)
-                }
-            })
-            task.resume()
-            
-            
+            self.pushFeedbackToServer(feedback)
         }
     }
     override func viewWillAppear(_ animated: Bool) {
@@ -127,6 +79,7 @@ class MasterViewController: UITableViewController, UISearchBarDelegate {
                         if !self.feedbacks.contains(where: {$0.text == text}) {
                             let newFeedback = Feedback(context: context)
                             newFeedback.text = text
+                            newFeedback.creationDate = ISO8601DateFormatter().date(from: element["creationDate"] as! String)
                             newFeedback.voteCounter = element["votes"] as! Int32
                         }
                     }
@@ -142,6 +95,30 @@ class MasterViewController: UITableViewController, UISearchBarDelegate {
             }
 
         }.resume()
+    }
+    
+    func pushFeedbackToServer(_ feedback: Feedback) {
+        let formatter = ISO8601DateFormatter()
+        let formattedDate = formatter.string(from: feedback.creationDate!)
+        let parameters = ["text": feedback.text!, "creationDate": formattedDate, "votes": feedback.voteCounter] as [String : Any]
+        
+        let url = URL(string: "https://fischli.pythonanywhere.com/postJSONparam/")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST" //set http method as POST
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        let task = URLSession.shared.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+            return
+        })
+        task.resume()
     }
     
     // MARK: - Segues
